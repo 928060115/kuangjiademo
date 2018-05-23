0add80ca600b92709ca8b2bc2107591a5d48f880
liuyang <liuyang@chengyiwm.com>
2018-05-23 18:10:38 +0800

    修改maven工程为gradle工程

---
+++ src/main/java/com/example/kuangjiademo/KuangjiademoApplication.java
@@ -0,0 +1,12 @@
+package com.example.kuangjiademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuangjiademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuangjiademoApplication.class, args);
+	}
+}
